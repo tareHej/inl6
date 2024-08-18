d3dbe966c3bf7e6d33cb0af67cb65ad601917da1
tareHej <128603982+tareHej@users.noreply.github.com>
2024-08-18 14:43:51 +0200

    Update page.js
    lagt till funktion för att sortera

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import MovieList from './components/MovieList';
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [sortBy, setSortBy] = useState('title');
 
   useEffect(() => {
     const storedMovies = JSON.parse(localStorage.getItem('movies')) || [];
@@ -24,12 +25,35 @@ const Home = () => {
     setMovies(newMovies);
   };
 
+
+  const sortMovies = (movies, option) => {
+    return [...movies].sort((a, b) => {
+      if (option === 'title') {
+        return a.title.localeCompare(b.title);
+      } else if (option === 'rating') {
+        return b.rating - a.rating;
+      }
+    });
+  };
+
+  const handleSortChange = (e) => {
+    setSortBy(e.target.value);
+  };
+
   return (
     <div>
       <h1>Min filmlista</h1>
       <UserInput addMovie={addMovie} />
+
+
       <h2>Inlagda filmer:</h2>
-      <MovieList movies={movies} removeMovie={removeMovie} />
+      <label>Sortera efter: </label>
+      <select value={sortBy} onChange={handleSortChange}>
+        <option value="title">Titel</option>
+        <option value="rating">Betyg</option>
+      </select>
+
+      <MovieList movies={sortMovies(movies, sortBy)} removeMovie={removeMovie} />
     </div>
   );
 };
